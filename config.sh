d93ffa0208813e3cebde36abdaafb68fd108f7cc
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-08-02 14:20:15 -0400

    Fixed configs and hooks

--- config.sh
+++ config.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Symlink the configuration to mutt's default search file
-MUTT_CONF=~/.muttrc
-if [ -f  $MUTT_CONF ]; then
+MUTT_CONF=$HOME/.muttrc
+echo $MUTT_CONF
+if [ -L  "$MUTT_CONF" ] || [ -f "$MUTT_CONF" ]; then
+    echo "Found file exists"
     rm $MUTT_CONF
 fi
 ln -s $(pwd).muttrc $MUTT_CONF
 
 # Enable the githooks to check for passwords and emails
-# before commiting them into the git history - Offf! :(
+# before commiting them into the git history - Ooff! :(
+chmod +x .githooks/*
 git config core.hooksPath .githooks
